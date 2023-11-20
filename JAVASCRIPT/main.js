2946082d367d256bf30f05631aea6081fa7d40cd
Phan Ngọc Thanh Thư <thanhthuh9593@gmail.com>
2023-11-20 21:12:03 +0700

    First commit

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,10 @@
+/*".btn_change"*/
+document.querySelector("#bg1").addEventListener("click",function(){
+    alert("Bạn vừa bấm vào thẻ bg1 ")
+    document.querySelector("#background_image img").src='./assets/imagefish/BACKGROUNDSEA.jpg'
+})
+document.querySelector("#bg2").addEventListener("click",function(){
+    alert("Bạn vừa bấm vào thẻ bg2 ")
+    document.querySelector("#background_image img").src='./assets/imagefish/PIC.webp'
+})
+
